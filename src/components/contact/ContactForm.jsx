6d1f6e8b787d1107b20473d1b9b9bb1bbe022f65
Melchor Callos <melchorcallos04@gmail.com>
2023-06-13 18:38:02 +0800

    Add: EmailJs for sending mail

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,30 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import {
   toastSuccess,
   toastWarning,
   toastError,
 } from "../../utils/toastHelper";
 
+const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+const serviceId = import.meta.env.VITE_SERVICE_ID;
+const templateId = import.meta.env.VITE_TEMPLATE_ID;
+
 const ContactForm = () => {
   const [isSending, setIsSending] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const form = useRef();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSending(true);
     if (!name || !email || !message) {
       toastWarning("Please complete all fields!");
+      setIsSending(false);
       return;
     }
-    setName("");
-    setEmail("");
-    setMessage("");
-    toastSuccess("Thank you! I will get back to you as soon as possible.");
+    emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+      (result) => {
+        console.log(result.text);
+        setIsSending(false);
+        setName("");
+        setEmail("");
+        setMessage("");
+        toastSuccess("Thank you! I will get back to you as soon as possible.");
+      },
+      (error) => {
+        console.log(error.text);
+        setIsSending(false);
+        toastError("Message failed! Something went wrong.");
+      }
+    );
   };
 
   return (
     <form
+      ref={form}
       className="lg:w-1/3 md:w-1/2 w-full mx-auto p-6 bg-gray-200 dark:bg-gray-800 rounded-lg shadow-md"
       onSubmit={handleSubmit}
     >
@@ -64,12 +84,12 @@ const ContactForm = () => {
         ></textarea>
       </div>
       <div className="flex justify-center items-center text-center">
-        <button
-          className="flex items-center justify-center bg-primary text-white font-bold py-3 px-8 rounded-lg transition duration-300 hover:bg-primary/60"
+        <input
+          className="flex items-center justify-center bg-primary text-white font-bold py-3 px-8 rounded-lg transition duration-300 hover:bg-primary/60 cursor-pointer"
           type="submit"
-        >
-          {isSending ? "LOADING..." : "SUBMIT"}
-        </button>
+          disabled={isSending}
+          value={isSending ? "SENDING..." : "SEND"}
+        />
       </div>
     </form>
   );
