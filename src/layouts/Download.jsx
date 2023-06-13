8427bc761e7266a733cd9daf2741449000dd2ecf
Melchor Callos <melchorcallos04@gmail.com>
2023-06-13 19:17:03 +0800

    Add: Social links and download cv

--- src/layouts/Download.jsx
+++ src/layouts/Download.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { preloaderData } from "../data/content";
 
+const cvUrl = import.meta.env.VITE_CV_URL;
+
 const Download = () => {
+  const handleDownload = () => {
+    const filename = cvUrl.split("/").pop();
+    const aTag = document.createElement("a");
+    aTag.href = cvUrl;
+    aTag.setAttribute("download", filename);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
+
   return (
     <div className="fixed bottom-0 right-0 p-3 z-50 mr-1">
-      <button className="text-primary font-bold flex items-center hover:animate-shake">
+      <button
+        className="text-primary font-bold flex items-center hover:animate-shake"
+        onClick={handleDownload}
+      >
         <img src={preloaderData.loader} className="w-10 h-10" />
         <span className="hidden sm:inline mr-[7px]">DOWNLOAD</span>
         <span>CV</span>
--- src/layouts/Socials.jsx
+++ src/layouts/Socials.jsx
@@ -5,14 +5,14 @@ const Socials = () => {
   return (
     <div className="fixed left-0 bottom-0 flex sm:flex-col flex-row sm:space-y-4 sm:space-x-0 space-x-4 p-4 z-50">
       <a
-        href="#"
+        href="https://linkedin.com/in/melchorcallos/"
         target="_blank"
         className="w-8 h-8 bg-primary hover:scale-125 duration-300 text-light dark:text-dark p-2 rounded-full flex items-center"
       >
         <FaLinkedin />
       </a>
       <a
-        href="#"
+        href="https://github.com/melchie04"
         target="_blank"
         className="w-8 h-8 bg-primary hover:scale-125 duration-300 text-light dark:text-dark p-2 rounded-full flex items-center"
       >
@@ -20,14 +20,14 @@ const Socials = () => {
       </a>
 
       <a
-        href="#"
+        href="https://facebook.com/melchorcallos04"
         target="_blank"
         className="w-8 h-8 bg-primary hover:scale-125 duration-300 text-light dark:text-dark p-2 rounded-full flex items-center"
       >
         <FaFacebook />
       </a>
       <a
-        href="#"
+        href="https://instagram.com/melchie.cs/"
         target="_blank"
         className="w-8 h-8 bg-primary hover:scale-125 duration-300 text-light dark:text-dark p-2 rounded-full flex items-center"
       >
